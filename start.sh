0a411dd999e5975e49cb27793f0ed7c9adf00c96
Boyd Hemphill <boyd@stackengine.com>
2015-09-22 12:32:09 -0500

    Enhance apache server respose issues#1
    Make it more obvious what the hash is for those not as familiar
    with Docker.

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 # The hostname will be the container short hash which will be different for
 # each container, thus enough to show, for example, a round robin balancing
 # strategy
-echo "<h1>$HOSTNAME</h1>" > /usr/local/apache2/htdocs/index.html
+echo "<h1>Container Hash: $HOSTNAME</h1>" > /usr/share/nginx/html/index.html
 
 # Use `exec` so that PID 1 is the nginx process and not this script
 exec /usr/local/bin/httpd-foreground
